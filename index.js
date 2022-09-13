b3e3e7177b86080351948b78e6978318a99af404
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-09-13 15:29:49 +1000

    Add array of knight movements.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
 
+const moveDiff = [
+    {x:1,y:2},
+    {x:2,y:1},
+    {x:2,y:-1},
+    {x:1,y:-2},
+    {x:-1,y:-2},
+    {x:-2,y:-1},
+    {x:-2,y:1},
+    {x:-1,y:2}
+];
+
+function knightsMoves() {
+
+}
